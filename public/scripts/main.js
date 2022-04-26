10c4655eb2d48ef54790792d0307e9e42c876654
Khaled Abo <khaledabo@Khaleds-MacBook-Pro.local>
2022-04-26 12:04:41 +0200

    Fix Closure issue

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,30 +1,27 @@
-
-
-import {io} from '/socket.io/socket.io.esm.min.js'
+import { io } from "/socket.io/socket.io.esm.min.js";
 let socket;
 
-connect.addEventListener('click', () =>{
-     socket = io()
-     socket.on('activate', (message) => {
-         send.disabled = false
-        console.log(message);
-    })
-})
-disconnect.addEventListener('click', () =>{
-    socket = null;
-    send.disabled = true
-    console.log('Logged out from chat!');
-})
-send.addEventListener('click', () =>{
-    socket.emit('message', message.value)
-    const li = document.createElement('li')
-    li.innerText = message.value
-    ul.appendChild(li)
-})
+connect.addEventListener("click", () => {
+  socket = io();
+  socket.on("activate", (message) => {
+    send.disabled = false;
+    console.log(message);
+  });
+  socket.on("messages", (messages) => {
+    console.log(messages);
+  });
+});
+disconnect.addEventListener("click", () => {
+  socket = null;
+  send.disabled = true;
+  console.log("Logged out from chat!");
+});
+send.addEventListener("click", () => {
+  socket.emit("message", message.value);
+  const li = document.createElement("li");
+  li.innerText = message.value;
+  ul.appendChild(li);
+});
 
-if(socket){
-    socket.on('messages', (messages) =>{
-        console.log(messages)
-    })
-
+if (socket) {
 }
